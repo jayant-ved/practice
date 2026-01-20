0f0a626dc9f708fac79f4d228830c6ac6178edb1
Jayant ved <jayantved27@gmail.com>
2026-01-20 17:51:56 +0530

    Palindrome code

--- Practice.py
+++ Practice.py
@@ -1,6 +1,14 @@
-num =[1,2,1]
-num2 = num.copy()
+list = []
+x = int(input ("enter: "))
+y = str(x)
+idx = 0
+while idx < len(y):
+    z= y[idx]
+    list.append(z)
+    idx += 1
+
+num2 = list.copy()
 num2.reverse()
-if (num == num2):
+if (list == num2):
     print ("palindrome")
 else: print ("not palindrome")
